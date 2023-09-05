8c4d05cbb47efb4b9c3f61b6751b6c517ea01dba
Lucas Marques <dev.lucashmarques@outlook.com>
2023-09-04 23:29:35 -0300

    Primeiro codigo(JAVA)

---
+++ JavaCompleto2023/primeiro/src/primeiro1.java
@@ -0,0 +1,10 @@
+
+public class primeiro1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá mundo!");
+
+	}
+
+}
---
+++ JavaCompleto2023/teste/src/teste/teste1.java
@@ -0,0 +1,12 @@
+package teste;
+
+public class teste1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá mundo!");
+		System.out.println("Chegueeeei!");
+
+	}
+
+}
