991a32019dcefff6f9e45a8dc89397292c7491e9
Lucas Marques <dev.lucashmarques@outlook.com>
2023-09-07 14:41:03 -0300

    testes

---
+++ JavaCompleto2023/enquanto_while/src/enquanto_while/Main.java
@@ -0,0 +1,24 @@
+package enquanto_while;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = 1;
+		
+		int soma = 0;
+		while (x != 0) {
+			x = sc.nextInt();
+			soma += x;
+		}
+		
+		System.out.println(soma);
+		
+		sc.close();
+	} 
+
+}
--- JavaCompleto2023/exemplo/src/module-info.java
+++ JavaCompleto2023/exemplo/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module a {
+module exemplo {
 }
